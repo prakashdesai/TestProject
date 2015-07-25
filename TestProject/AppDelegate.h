317f36bc2807f39894e72a3afd770a6997795595
ajaydesai_mca <ajaydesai_mca@yahoo.co.in>
2015-07-25 20:07:07 +0530

    first commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by ajay desai on 25/07/15.
+//  Copyright (c) 2015 Fluxtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by ajay desai on 25/07/15.
+//  Copyright (c) 2015 Fluxtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
